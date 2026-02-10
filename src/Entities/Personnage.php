df60463509aa7789e14ef6c74c5806caa6e4ae04
mikaT <m.tores63@outlook.fr>
2026-02-10 17:00:06 +0100

    completed : getters and setters in Perosonnage entity ok

--- src/Entities/Personnage.php
+++ src/Entities/Personnage.php
@@ -7,4 +7,42 @@ abstract class Personnage
     {
 
     }
+
+    public function getHp(): Int
+    {
+        return $this->hp;
+    }
+
+       public function setHp(int $health): self
+    {
+        $this->hp  = $health;
+
+        return $this;
+    }
+
+
+    public function getAtk(): Int
+    {
+        return $this->atk;
+    }
+
+       public function setAtk(int $attack): self
+    {
+        $this->atk  = $attack;
+
+        return $this;
+    }
+
+
+    public function getDef(): Int
+    {
+        return $this->def;
+    }
+
+    public function setDef(int $defense): self
+    {
+        $this->def  = $defense;
+
+        return $this;
+    }
 }
