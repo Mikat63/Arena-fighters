f2a48b59ea24af7cba6c1fee74ada088d149dfb9
mikaT <m.tores63@outlook.fr>
2026-02-11 15:20:38 +0100

    COMPLETED : install tailwindCss

---
+++ arena_fighters_full.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 11 fév. 2026 à 14:11
+-- Version du serveur : 9.1.0
+-- Version de PHP : 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `arena_fighters`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `monsters`
+--
+
+DROP TABLE IF EXISTS `monsters`;
+CREATE TABLE IF NOT EXISTS `monsters` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  `hp` bigint NOT NULL,
+  `atk` bigint NOT NULL,
+  `def` bigint NOT NULL,
+  `type` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `monsters`
+--
+
+INSERT INTO `monsters` (`id`, `Name`, `hp`, `atk`, `def`, `type`) VALUES
+(1, 'Orc Grinçant', 55, 12, 5, 'orc'),
+(2, 'Orc Éclaireur', 65, 14, 6, 'orc'),
+(3, 'Orc Berserker', 75, 18, 5, 'orc'),
+(4, 'Golem de Terre', 130, 14, 13, 'golem'),
+(5, 'Golem de Pierre', 120, 12, 12, 'golem'),
+(6, 'Golem Runique', 110, 16, 14, 'golem'),
+(7, 'Dragon Rouge', 140, 18, 9, 'dragon'),
+(8, 'Dragons des Cendres', 170, 20, 11, 'dragon'),
+(9, 'Dragon Ancien', 200, 22, 12, 'dragon'),
+(10, 'Dragon Noir', 190, 25, 11, 'dragon');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
